be3e19ae08f673b42dcf6e62af696cbd6f5be7c9
toasted-coder <christmanholmes21@gmail.com>
2021-05-13 13:12:30 -0700

    Initial commit

---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASEURL = "https://randomuser./api/?results=20";
+
+const API = {
+  search: function () {
+    return axios.get(BASEURL);
+  },
+};
+
+export default API;
