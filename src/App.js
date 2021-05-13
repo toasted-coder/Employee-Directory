306b14ac83aa2d22963021e6b601476dd5d0c71f
toasted-coder <christmanholmes21@gmail.com>
2021-05-13 14:10:54 -0700

    added searchUser, handleInputChange, handleFormSubmit, componentDidMount, and state for App.js

--- src/App.js
+++ src/App.js
@@ -4,4 +4,53 @@ import "./App.css";
 import resultsList from "./components/resultsList";
 import searchForm from "./components/searchForm";
 
+class App extends React.Component {
+  state = {
+    search: "",
+    employeeList: [],
+    filteredEmployees: [],
+    sortedLastName: [],
+  };
+
+  componentDidMount() {
+    this.searchUser();
+  }
+
+  searchUser = () => {
+    API.search()
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          employeeList: res.data.results,
+          filteredEmployees: res.data.results,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleInputChange = (event) => {
+    const value = event.target.value.trim();
+    this.setState({
+      search: value,
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.employeeList);
+    const sortedLastName = this.state.employeeList.sort((a, b) => {
+      if (a.name.last > b.name.last) {
+        return 1;
+      } else if (b.name.last > a.name.last) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    this.setState({
+      sortedLastName: sortedLastName,
+    });
+  };
+}
+
 export default App;
